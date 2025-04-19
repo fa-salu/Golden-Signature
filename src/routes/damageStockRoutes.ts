057d906e64187edb16e947e4b9d78ecd29dbf88f
Hamzathul Favas E <83232962+hamzathul@users.noreply.github.com>
2025-04-19 11:48:28 +0530

    vehicle and damage stocks controllers
    * group controller
    
    * add route, vehicle and party controller
    
    * add bank, bank entry, party, item, payment, receipt, category, tax, vehicleStock controllers
    
    * damageStock and vehicleStock controllers

--- src/routes/damageStockRoutes.ts
+++ src/routes/damageStockRoutes.ts
@@ -2,26 +2,26 @@ import express from "express";
 import { verifyToken } from "../middlewares/verifyToken";
 import { authorizeRoles } from "../middlewares/roleMiddleware";
 import { validateData } from "../middlewares/zodValidation";
-import { vehicleStockSchema } from "../utils/zodSchema";
+import { damageStockSchema } from "../utils/zodSchema";
 import { errorCatch } from "../utils/error/error.Catch";
-import { createVehicleStock, updateVehicleStock } from "../controllers/vehicleStockController";
+import { createDamageStock, updateDamageStock } from "../controllers/damageStockController";
 
 const router = express.Router();
 
 router.post(
-  "/vehiclestock/create",
+  "/damagestock/create",
   verifyToken,
   authorizeRoles("admin"),
-  validateData(vehicleStockSchema),
-  errorCatch(createVehicleStock)
+  validateData(damageStockSchema),
+  errorCatch(createDamageStock)
 );
 
 router.put(
-  "/vehiclestock/update/:id",
+  "/damagestock/update/:id",
   verifyToken,
   authorizeRoles("admin"),
-  validateData(vehicleStockSchema),
-  errorCatch(updateVehicleStock)
+  validateData(damageStockSchema),
+  errorCatch(updateDamageStock)
 );
 
 export default router;
